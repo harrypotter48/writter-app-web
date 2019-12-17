e249937b5217b5893ffb27b570a945efe904efa9
Harry Potter <cabrerarobles@gmail.com>
2019-12-17 15:51:27 -0500

    Version 1.0

---
+++ precache-manifest.b17440321c1a6e21e7ae1ecec8dea128.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e010f075297743228d1f1e372b3142",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e7f2be23e01d17e940b",
+    "url": "/static/css/2.c0f5ac91.chunk.css"
+  },
+  {
+    "revision": "6e7f2be23e01d17e940b",
+    "url": "/static/js/2.2d7f2358.chunk.js"
+  },
+  {
+    "revision": "a7407b19490479776ffe",
+    "url": "/static/js/main.0bfca568.chunk.js"
+  },
+  {
+    "revision": "0f52c4b3ef166a910669",
+    "url": "/static/js/runtime-main.d930e5dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
